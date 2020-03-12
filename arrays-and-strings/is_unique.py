b760a3b8154b231ebd91ca3f78c16bb97a08ecf0
carterk <carterk@sfu.ca>
2020-03-11 17:28:48 -0700

    fix and finish is_unique.py

--- arrays-and-strings/is_unique.py
+++ arrays-and-strings/is_unique.py
@@ -2,37 +2,52 @@
 # Is Unique: Implement an algorithm to determine if a string has all unique characters.
 # What if you cannot use additional data structures?
 
-# brute-force algorithm - O(n^2)
-def check_unique(some_string):
-    for i in range(0, len(some_string) - 1):
-        for j in range(i + 1, len(some_string) - 1):
-            if some_string[i] == some_string[j]:
+# brute-force algorithm - O(n^2) time, O(1) space
+def is_unique(str):
+    if (len(str) > 128): # max number of unique characters
+        return False
+
+    for i in range(len(str) - 1):
+        for j in range(i + 1, len(str) - 1):
+            if str[i] == str[j]:
                 return False
 
     return True
 
-assert(check_unique("hello") == False)
-assert(check_unique("world") == True)
+assert(is_unique("hello") == False)
+assert(is_unique("world") == True)
 
-print("Test brute-force algorithm:")
-print("hello - " + str(check_unique("hello")))
-print("world - " + str(check_unique("world")))
+# pre-sort string - O(nlogn), O(1) or more space
+def is_unique2(str):
+    if (len(str) > 128): # max number of unique characters
+        return False
 
-# pre-sort string - O(nlogn)
-def check_unique2(some_string):
-    sorted_string = sorted(some_string) # some nlogn sort
+    sorted_str = sorted(str) # some nlogn sort
 
-    for i in range(0, len(sorted_string) - 1):
-        if sorted_string[i] == sorted_string[i + 1]:
+    for i in range(len(sorted_str) - 1):
+        if sorted_str[i] == sorted_str[i + 1]:
             return False
 
     return True
 
-assert(check_unique2("hello") == False)
-assert(check_unique2("world") == True)
+assert(is_unique2("hello") == False)
+assert(is_unique2("world") == True)
 
-print("Test pre-sorted string algorithm:")
-print("hello - " + str(check_unique2("hello")))
-print("world - " + str(check_unique2("world")))
+# using an additional data structure - O(n) time, O(1) space
+def is_unique3(str):
+    if (len(str) > 128): # max number of unique characters
+        return False
 
-# using an additional data structure - O(n)
+    ascii_dict = {chr(i): False for i in range(129)}
+
+    for i in range (0, len(str) - 1):
+        if ascii_dict[str[i]]:
+            return False
+        ascii_dict[str[i]] = True
+    
+    return True
+
+assert(is_unique3("hello") == False)
+assert(is_unique3("world") == True)
+
+print("Passed.")
