0d905296b044d34156b1c52550ca0cffa176a5de
carterk <carterk@sfu.ca>
2020-03-16 14:40:20 -0700

    is_unique: fix ranges, use input_str

--- arrays-and-strings/is_unique.py
+++ arrays-and-strings/is_unique.py
@@ -3,13 +3,13 @@
 # What if you cannot use additional data structures?
 
 # brute-force algorithm - O(n^2) time, O(1) space
-def is_unique(str):
-    if (len(str) > 128): # max number of unique characters
+def is_unique(input_str):
+    if (len(input_str) > 128): # max number of unique characters
         return False
 
-    for i in range(len(str) - 1):
-        for j in range(i + 1, len(str) - 1):
-            if str[i] == str[j]:
+    for i in range(len(input_str)):
+        for j in range(i + 1, len(input_str)):
+            if input_str[i] == input_str[j]:
                 return False
 
     return True
@@ -18,14 +18,14 @@ assert(is_unique("hello") == False)
 assert(is_unique("world") == True)
 
 # pre-sort string - O(nlogn) time, O(1) or more space
-def is_unique2(str):
-    if (len(str) > 128): # max number of unique characters
+def is_unique2(input_str):
+    if (len(input_str) > 128): # max number of unique characters
         return False
 
-    str_sorted = sorted(str) # some nlogn sort
+    sorted_str = sorted(input_str) # some nlogn sort
 
-    for i in range(len(str_sorted) - 1):
-        if str_sorted[i] == str_sorted[i + 1]:
+    for i in range(len(sorted_str) - 1):
+        if sorted_str[i] == sorted_str[i + 1]:
             return False
 
     return True
@@ -34,16 +34,16 @@ assert(is_unique2("hello") == False)
 assert(is_unique2("world") == True)
 
 # using an additional data structure - O(n) time, O(1) space
-def is_unique3(str):
-    if (len(str) > 128): # max number of unique characters
+def is_unique3(input_str):
+    if (len(input_str) > 128): # max number of unique characters
         return False
 
     ascii_dict = {chr(i): False for i in range(129)}
 
-    for i in range(len(str) - 1):
-        if ascii_dict[str[i]]:
+    for i in range(len(input_str)):
+        if ascii_dict[input_str[i]]:
             return False
-        ascii_dict[str[i]] = True
+        ascii_dict[input_str[i]] = True
     
     return True
 
