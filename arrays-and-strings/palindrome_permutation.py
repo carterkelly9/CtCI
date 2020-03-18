0bffa730f959c88d2cbde44197126e9ad652861d
carterk <carterk@sfu.ca>
2020-03-18 15:44:00 -0700

    arrays-and-strings: add palindrome_permutation

---
+++ arrays-and-strings/palindrome_permutation.py
@@ -0,0 +1,29 @@
+# 1.4
+# Palindrome Permutation: Given a string, write a function to check if it is a permutation of a palindrome.
+# A palindrome is a word or phrase that is the same forwards as backwards.
+# A permutation is a rearrangement of letters.
+# The palindrome does not need to be limited to just dictionary words.
+
+# using an additional data structure - O(n) time, O(1) space
+def palindrome_permutation(input_str):    
+    ascii_dict = {chr(i): 0 for i in range(129)}
+    
+    for i in input_str:
+        ascii_dict[i] += 1
+    
+    one_odd = False
+
+    for i in ascii_dict.keys():
+        if (ascii_dict[i] % 2 == 1):
+            if (one_odd == True):
+                return False
+            one_odd = True
+    
+    return True
+ 
+assert(palindrome_permutation("hello") == False)
+assert(palindrome_permutation("even") == False)
+assert(palindrome_permutation("odd") == True)
+assert(palindrome_permutation("evenvn") == True)
+
+print("Passed.")
